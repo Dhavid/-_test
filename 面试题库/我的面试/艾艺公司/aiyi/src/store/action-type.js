61a5b12dfac6d52c4d71b1f0cebe299439f40a8c
杜海维 <duhaiwei@duhaiweideiMac.local>
2020-04-09 10:43:53 +0800

    q

--- 面试题库/我的面试/艾艺公司/aiyi/src/store/action-type.js
+++
@@ -1,4 +0,0 @@
-export default {
-    PUBLIC_PUSH:"PUBLIC_PUSH",
-    PUBLIC_ADD:"PUBLIC_ADD"
-}
--- 面试题库/我的面试/艾艺公司/aiyi/src/store/actions/test.js
+++
@@ -1,8 +0,0 @@
-import Types from "../action-type.js";
-
-export default {
-    push(id){
-        return {type:types.PUBLIC_PUSH}
-    }
-    
-}
